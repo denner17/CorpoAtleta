017768ed894e945e6ffbd25f112ba4e3daecd6c2
Denner dos Santos <dennersimao2@gmail.com>
2025-01-06 16:48:21 -0300

    update /texts/carrossel

--- main.js
+++ main.js
@@ -8,3 +8,28 @@ function menuOnClick() {
 
 
 
+const carousel = document.querySelector('.carousel');
+const images = document.querySelectorAll('.carousel img');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+    const width = images[currentIndex].clientWidth;
+    carousel.style.transform = `translateX(-${currentIndex * width}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+});
+
+// Ajusta o carrossel ao redimensionar a janela
+window.addEventListener('resize', updateCarousel);
+
